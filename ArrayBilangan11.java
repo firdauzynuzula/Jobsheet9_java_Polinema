7ca2e55c152209b5ffc91c65b5d274c1bcb6db1f
firdauzy <nuzulafirdhauzy@gmail.com>
2025-11-13 16:00:28 +0700

    modifikasi percobaan 1

--- ArrayBilangan11.java
+++ ArrayBilangan11.java
@@ -1,8 +1,12 @@
 public class ArrayBilangan11 {
     public static void main(String[] args) {
-        int[] bil = {1,2,3,4,5,6,7,8,9,10};
+        int[] bil = new int[4];
 
-        for (int i = 0; i < 4; i++) {
+        bil[0] = 5;
+        bil[1] = -13;
+        bil[2] = -7;
+        bil[3] = 17;
+        for (int i = 0; i <= 4; i++) {
             System.out.println(bil[i]);
         }
     }
