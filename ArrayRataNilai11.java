f8f2228932d1dd1147596b95957cb71485e68148
firdauzy <nuzulafirdhauzy@gmail.com>
2025-11-14 10:44:12 +0700

    update ArrayRataNilai11: tambah array integer

---
+++ ArrayRataNilai11.java
@@ -0,0 +1,12 @@
+public class ArrayRataNilai11 {
+    public static void main(String[] args) {
+        // Contoh array bertipe integer
+        int[] nilaiAkhir = {80, 85, 90, 75, 88, 92, 78, 84, 89, 95};
+
+        System.out.println("Isi array nilaiAkhir:");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.println("Nilai ke-" + (i + 1) + ": " + nilaiAkhir[i]);
+        }
+    }
+}
+
